9dc298f9dffb37aa4853a08807afb22f16a90eb6
HAiGOU <null@null.null>
2025-09-06 01:53:58 -0500

    as2

--- picasso2.py
+++ picasso2.py
@@ -357,7 +357,7 @@ class CameraInterface:
                 if new_size == original_size:
                     self.logger.log(f"File move complete: {to_path}")
                     self.metadata['saving']['moved_bytes'] = new_size
-                    self.metadata['saving']['complete'] = True
+                    self.metadata['saving']['complete'] = False
                     break
                 self.metadata['saving']['moved_bytes'] = new_size
             # if time.time() - start_time > 30: # this is bad because some drives are slow
